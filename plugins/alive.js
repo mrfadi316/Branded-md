c599288d6d8c36760bac8df7f268932c8afffa73
Mr Fadi <100305289+mrfadi316@users.noreply.github.com>
2024-12-01 21:34:49 +0500

    Add files via upload

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,21 +1,20 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-const {sleep} = require('../lib/functions')
 
 cmd({
-    pattern: "restart",
-    desc: "restart the bot",
-    category: "owner",
+    pattern: "alive",
+    desc: "Check bot online or no.",
+    category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-const {exec} = require("child_process")
-reply("restarting...")
-await sleep(1500)
-exec("pm2 restart all")
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
+
+
+
