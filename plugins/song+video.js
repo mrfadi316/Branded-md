e4cdbcb6a21998aeb84faae0e11b4e5423f5d0c9
Mr Fadi <100305289+mrfadi316@users.noreply.github.com>
2024-12-03 09:54:25 +0500

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,6 +1,8 @@
 const {cmd , commands} = require('../command')
 const fg= require('api-dylux')
 const yts= require('yt-search')
+
+
 cmd({
     pattern: "song",
     desc: "download songs",
@@ -9,6 +11,8 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+if (q)
+const search = await yts(q)
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
