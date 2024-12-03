41b186821424b6d08c1d2852b355a49d5daaa26d
Mr Fadi <100305289+mrfadi316@users.noreply.github.com>
2024-12-03 13:31:32 +0500

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -4,8 +4,8 @@ const yts= require('yt-search')
 
 
 cmd({
-    pattern: "song",
-    desc: "download songs",
+    pattern: "audio",
+    desc: "download audio",
     category: "download",
     filename: __filename
 },
@@ -16,7 +16,7 @@ const search = await yts(q)
 const data = search.videos[0];
 const url = data.url
 
-let desc = '
+let desc = `
     ⬇️ *BRANDED MD AUDIO DOWNLOADER* ⬇️
 
 title:  ${data.title}
@@ -26,7 +26,7 @@ ago: ${data.ago}
 views: ${data.views}
 
 MADE BY *MR FADI*
-'
+`
 await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
     
 //download audio
@@ -34,10 +34,70 @@ await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:m
 let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
-//send audio message
+//send audio + document message
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-const url 
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+ await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title +".mp3", caption: "*Made By > Mr Fadi*"},{quoted:mek})
+
+
+
+    
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+//===========video-dl========
+
+
+cmd({
+    pattern: "video",
+    desc: "download video",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if (!q) return reply ("Give me URL or Title")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+    ⬇️ *BRANDED MD VIDEO DOWNLOADER* ⬇️
+
+Title:  ${data.title}
+
+Description: ${data.description}
+
+Time: ${data.timestamp}
+
+ago: ${data.ago}
+
+views: ${data.views}
+
+
+Join My Channel For Updates
+
+ _https://whatsapp.com/channel/0029VatIItrD38CVaysgC42O_
+
+
+> //POWERED BY *MR FADI* 
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+    
+//download video
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video + document message
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+ await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title +".mp4",caption: "*Made By > Mr Fadi*"},{quoted:mek})
+
+
+
+    
 }catch(e){
 console.log(e)
 reply(`${e}`)
