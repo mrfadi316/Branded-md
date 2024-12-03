26d9dd8694903b5a9922f421446e07cbaafc6228
Mr Fadi <100305289+mrfadi316@users.noreply.github.com>
2024-12-03 13:57:14 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://qu.ax/LTHEX.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HELLO I AM BRANDED-MD ONLINE NOW"
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLO I AM BRANDED-MD ONLINE NOW",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
