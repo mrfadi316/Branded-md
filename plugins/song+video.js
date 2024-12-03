e593037b0a7b1076cb61f5dcc1f63945b7bf61e0
Mr Fadi <100305289+mrfadi316@users.noreply.github.com>
2024-12-03 09:36:21 +0500

    Create song+video.js

---
+++ plugins/song+video.js
@@ -0,0 +1,17 @@
+const {cmd , commands} = require('../command')
+const fg= require('api-dylux')
+const yts= require('yt-search')
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
