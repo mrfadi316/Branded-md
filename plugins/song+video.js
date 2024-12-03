71fef132a6e087244f87a944f050479861a96ed2
Mr Fadi <100305289+mrfadi316@users.noreply.github.com>
2024-12-03 11:41:10 +0500

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -14,6 +14,28 @@ try{
 if (!q) return reply ("Give me URL or Title")
 const search = await yts(q)
 const data = search.videos[0];
+const url = data.url
+
+let desc = '
+    *BRANDED MD AUDIO DOWNLOADER*
+
+title:  ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+MADE BY *MR FADI*
+'
+await conn,sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+    
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
 const url 
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
